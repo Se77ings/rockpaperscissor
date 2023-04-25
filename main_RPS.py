a8760a7f9eb674722e57e560fced225348c43502
Se77ings <129302174+Se77ings@users.noreply.github.com>
2023-04-25 12:35:28 -0300

    Test 2 pull request
    P2 movement

--- main_RPS.py
+++ main_RPS.py
@@ -1,1 +1,6 @@
-p1 = input("type 'R', 'P', or 'S' to make a move.
+import random
+
+mov = ['R','P','S']
+p1 = input("type 'R', 'P', or 'S' to make a move.")
+p2 = mov[random.randint(0,2)]
+print(p2)
